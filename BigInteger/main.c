db4b43a5392e3482dfac752bc7d451545276b534
Anton <stady.mail.itmo@gmail.com>
2021-10-04 17:19:52 +0300

    Initial Commit

---
+++ BigInteger/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BigInteger
+//
+//  Created by Антон Чемодуров on 04.10.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
